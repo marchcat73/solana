ae09b4faa0639c01dafab99ecdeeaf19b6e158f6
Vitaliy Vanyushin <wm@codehtml.ru>
2024-07-14 23:24:04 +0400

    navbar layout

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,13 +1,31 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @next/next/no-img-element */
 'use client';
 import { usePathname } from 'next/navigation';
-import { Disclosure, Menu, Transition } from '@headlessui/react';
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { useReactiveVar } from '@apollo/client';
 import cn from 'classnames';
+import { balanceVar } from '@app/app/cache';
 import { NavbarProps } from './Navbar.props';
 import styles from './Navbar.module.css';
 
 export default function Navbar({ className }: NavbarProps) {
-  return <nav className={cn('bg-gray-800', styles.navbar, className)}>1</nav>;
+  const pathname = usePathname();
+  const balance = useReactiveVar(balanceVar);
+  return (
+    <nav
+      className={cn(
+        'bg-gray-800 flex items-center justify-between',
+        styles.navbar,
+        className,
+      )}
+    >
+      <div className="text-white pl-3">
+        {pathname !== '/' ? (
+          <Link href="/">Home</Link>
+        ) : (
+          <button>Create Wallet</button>
+        )}
+      </div>
+      <div className="text-white pr-3">Balance: {balance} SOL</div>
+    </nav>
+  );
 }
