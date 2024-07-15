dd642ca7602e72428163f9fa3f6319bd61c909c9
Vitaliy Vanyushin <wm@codehtml.ru>
2024-07-15 12:17:35 +0400

    home

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -4,10 +4,28 @@ import { accountVar } from '@app/app/cache';
 
 const Home = () => {
   const account = useReactiveVar(accountVar);
+
   return (
     <div>
       <div>publicKey: {account && <p>{account.publicKey.toBase58()}</p>}</div>
-      <div>secretKey: {account && <p>{account.secretKey}</p>}</div>
+      <div>
+        secretKey:{' '}
+        {account && (
+          <>
+            <span>[</span>
+            <p>
+              {Array.from(account.secretKey).map((el, index, array) => {
+                if (index === array.length - 1) {
+                  return <span key={`${el}-${index}`}>{el}</span>;
+                } else {
+                  return <span key={`${el}-${index}`}>{el},</span>;
+                }
+              })}
+            </p>
+            <span>]</span>
+          </>
+        )}
+      </div>
     </div>
   );
 };
